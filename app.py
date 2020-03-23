010f7ad89919f72004683efb91e6817c0669ab15
Shivam Sharma <jack17529@users.noreply.github.com>
2020-03-24 04:13:28 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
