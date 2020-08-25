84ad1230b00df9837358f31ff17b19bb8e370d1a
Shivam Sharma <jack17529@users.noreply.github.com>
2020-08-25 23:58:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
+"""
+Creates a flask app using a template website.
+"""
 import requests
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+APP = Flask(__name__)
+APP.config['DEBUG'] = True
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+DB = SQLAlchemy(APP)
 
-class City(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+class City(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(50), nullable=False)
 
-@app.route('/', methods=['GET', 'POST'])
+@APP.route('/', methods=['GET', 'POST'])
 def index():
+    """Checks the request made is POST and commits else gets the data of the cities already
+    present in the sqlite database.
+
+    Returns:
+        render_template : returns the rendered template. 
+    """
+    
     if request.method == 'POST':
         new_city = request.form.get('city')
 
         if new_city:
             new_city_obj = City(name=new_city)
 
-            db.session.add(new_city_obj)
-            db.session.commit()
+            DB.session.add(new_city_obj)
+            DB.session.commit()
 
     cities = City.query.all()
 
@@ -32,13 +42,13 @@ def index():
 
     for city in cities:
 
-        r = requests.get(url.format(city.name)).json()
+        req_city = requests.get(url.format(city.name)).json()
 
         weather = {
             'city' : city.name,
-            'temperature' : r['main']['temp'],
-            'description' : r['weather'][0]['description'],
-            'icon' : r['weather'][0]['icon'],
+            'temperature' : req_city['main']['temp'],
+            'description' : req_city['weather'][0]['description'],
+            'icon' : req_city['weather'][0]['icon'],
         }
 
         weather_data.append(weather)
@@ -47,4 +57,4 @@ def index():
     return render_template('weather.html', weather_data=weather_data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    APP.run(host='0.0.0.0', port=5000, debug=True)
