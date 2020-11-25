4a1f42afce915552a0e0e1303f5c2ffe99f6e65b
jack <15ucs130@lnmiit.ac.in>
2020-11-25 16:51:43 +0530

    fmt .tf file

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-    region = "us-east-1"
-    version = "~> 3.17.0"
+  region  = "us-east-1"
+  version = "~> 3.17.0"
 }
 
 resource "aws_instance" "web1" {
-    ami           = ami-0885b1f6bd170450c
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
-    key_name = "jack-HP"
+  ami               = "ami-0885b1f6bd170450c"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "jack-HP"
 
-    # need to open port 8080 for jenkins
-    user_data = <<-EOF
+  # need to open port 8080 for jenkins
+  user_data = <<-EOF
                 #!/bin/bash
                 sudo apt-get update -y
                 sudo apt install git -y
@@ -21,18 +21,18 @@ resource "aws_instance" "web1" {
                 sudo apt install jenkins
                 EOF  
 
-    tags = {
-        Name = "jenkins-master"
-    }  
+  tags = {
+    Name = "jenkins-master"
+  }
 }
 
 resource "aws_instance" "web2" {
-    ami           = ami-0885b1f6bd170450c
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
-    key_name = "jack-HP"
+  ami               = "ami-0885b1f6bd170450c"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "jack-HP"
 
-    user_data = <<-EOF
+  user_data = <<-EOF
                 #!/bin/bash
                 sudo apt-get update -y
                 sudo apt install apache2 -y
@@ -44,18 +44,18 @@ resource "aws_instance" "web2" {
                 sudo systemctl start docker
                 EOF
 
-    tags = {
-        Name = "staging"
-    }    
+  tags = {
+    Name = "staging"
+  }
 }
 
 resource "aws_instance" "web3" {
-    ami           = ami-0885b1f6bd170450c
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
-    key_name = "jack-HP"
+  ami               = "ami-0885b1f6bd170450c"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "jack-HP"
 
-    user_data = <<-EOF
+  user_data = <<-EOF
                 #!/bin/bash
                 sudo apt-get update -y
                 sudo apt install apache2 -y
@@ -66,21 +66,21 @@ resource "aws_instance" "web3" {
                 sudo apt install docker-ce
                 sudo systemctl start docker
                 EOF
-    
-    tags = {
-        Name = "production"
-    }    
+
+  tags = {
+    Name = "production"
+  }
 }
 
 #resource "aws_security_group" "web" {
- #   name = "Web Security Group"
-  #  description = "Allow access to our web server"
+#   name = "Web Security Group"
+#  description = "Allow access to our web server"
 #
- #   ingress{
-  #      description = "Allow SSH"
-   #     from_port = 22
-    #    to_port = 22
-     #   protocol = "tcp"
-      #  cidr_blocks = ["0.0.0.0/0"]
-    #}
+#   ingress{
+#      description = "Allow SSH"
+#     from_port = 22
+#    to_port = 22
+#   protocol = "tcp"
+#  cidr_blocks = ["0.0.0.0/0"]
+#}
 #}
